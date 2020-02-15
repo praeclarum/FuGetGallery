7769beda187d29d6f685a809cfa438c6926b24fd
Bradley Grainger <bgrainger@gmail.com>
2020-02-15 10:10:52 -0800

    Support SemVer 2 packages. Fixes #75
    Also upgrade to "registration4" endpoint, which includes more package versions (see #95).

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -40,7 +42,10 @@ namespace FuGetGallery
             });
             services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
             services.AddResponseCompression();
-            services.AddHttpClient();
+            services.AddHttpClient ("")
+                .ConfigurePrimaryHttpMessageHandler (() => new HttpClientHandler {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
